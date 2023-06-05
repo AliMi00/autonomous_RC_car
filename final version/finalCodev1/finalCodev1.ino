4463d5cc77af4c0e0164739387b1f92d9502866e
Ali Mobini <alimobinidev@gmail.com>
2023-06-05 19:25:10 +0200

    whitout obs with stop end tested

--- final version/finalCodev1/finalCodev1.ino
+++ final version/finalCodev1/finalCodev1.ino
@@ -6,6 +6,7 @@
 #include <QTRSensors.h>
 #include "Adafruit_VL53L0X.h"
 #include <Servo.h>
+#include <PID_v1.h>
 
 
 
@@ -14,7 +15,7 @@
 //global var for enabling functions
 #pragma region virtual_delays
 unsigned long motorStartStrong = 150;
-unsigned long loopObjectDuration = 6000;
+unsigned long loopObjectDuration = 12000;
 unsigned long loopObjectCurrent = 0;
 unsigned long loopObjectStart = 0;
 unsigned long rampDurationStart = 0;
@@ -26,8 +27,11 @@ unsigned long rampDuration = 1000;
 
 
 bool activeCalibraton = false;
-int objectLoopDirection = 45;
-int objectLoopReverseDirection = 135;
+int objectLoopDirection = 135;
+int objectLoopReverseDirection = 45;
+bool stopCar = false;
+
+
 
 
 #pragma region Global_var
@@ -36,20 +40,44 @@ int objectLoopReverseDirection = 135;
 int QtrPosition = -1;
 int speed = 35;
 int objectDistance = 200;
-int rampDistance = 350;
-int tofRampdif = 30;
-int tofObjectDif = 100;
-int qtrMinValForWhiteLine = 500;
+int rampDistance = 30;
+int tofRampdif = 20;
+int tofObjectDif = 50;
+int qtrMinValForWhiteLine = 200;
+
+int pidOutput=90;
 
 Servo myservo;
 //QTR
 QTRSensors qtr;
 const uint8_t SensorCount = 8;
 uint16_t sensorValues[SensorCount];
-uint16_t qtrCalManMin[SensorCount] = {188,188,188,188,188,188,188,188};
+uint16_t qtrCalManMin[SensorCount] = {320,320,320,320,320,320,320,320};
 uint16_t qtrCalManMax[SensorCount] = {2500,2500,2500,2500,2500,2500,2500,2500};
 uint16_t qtrCalMin[SensorCount] = {0};
 uint16_t qtrCalMax[SensorCount] = {2500};
+
+
+// Set the desired position on the line (e.g., center position)
+const int desiredPosition = SensorCount * 1000 / 2;
+
+
+
+// Define PID constants
+const double Kp = 0.2;  // Proportional constant
+const double Ki = 0.1;  // Integral constant
+const double Kd = 0.1;  // Derivative constant
+
+// Variables for PID control
+int lastError = 0;
+int integral = 0;
+
+double input, output, setpoint;
+PID pid(&input, &output, &setpoint, Kp, Ki, Kd, DIRECT);
+
+//button
+
+byte lastButtonState = 0;
 
 //TOF
 // address we will assign if dual sensor is present
@@ -96,6 +124,8 @@ const uint8_t QTR_SENSORS_PINS[] = {A0,A1,A3,A4,A5,A6,A7,A2};
 #define SHT_LOX1 24
 #define SHT_LOX2 25
 
+#define BUTTON_PIN 30
+
 #pragma endregion
 
 long ultra1GetDistance(){
@@ -106,7 +136,7 @@ long ultra1GetDistance(){
    digitalWrite(ULTRA_1_PING_PIN, HIGH);
    delayMicroseconds(10);
    digitalWrite(ULTRA_1_PING_PIN, LOW);
-   pinMode(echoPin, INPUT);
+   pinMode(ULTRA_1_ECHO_PIN, INPUT);
    duration = pulseIn(ULTRA_1_ECHO_PIN, HIGH);
    cm = duration / 29 / 2;
    return cm;
@@ -121,7 +151,7 @@ long ultra2GetDistance(){
    digitalWrite(ULTRA_2_PING_PIN, HIGH);
    delayMicroseconds(10);
    digitalWrite(ULTRA_2_PING_PIN, LOW);
-   pinMode(echoPin, INPUT);
+   pinMode(ULTRA_1_ECHO_PIN, INPUT);
    duration = pulseIn(ULTRA_2_ECHO_PIN, HIGH);
    cm = duration / 29 / 2;
    return cm;
@@ -245,25 +275,50 @@ void servoMove(int degree){
     Serial.println("!!!!!! ******** ---------> wrong value for servo");
 }
 
+// Function to calculate the PID output
+double calculatePID(int position)
+{
+  int error = position - desiredPosition;
+  integral += error;
+  int derivative = error - lastError;
+  lastError = error;
+  
+  double pid = Kp * error + Ki * integral + Kd * derivative;
+  Serial.println(pid);
+  return pid;
+}
 
+void pidInit(){
+  pid.SetMode(AUTOMATIC);
+  pid.SetOutputLimits(0, 7000);
+  pid.SetSampleTime(50);
+}
 
 void readLine(){
     // read calibrated sensor values and obtain a measure of the line position
   // from 0 to 5000 (for a white line, use readLineWhite() instead)
+  int tempPosition = 0;
+  for(int i=0;i< 1 ; i++){
+    tempPosition += qtr.readLineWhite(sensorValues);
+  }
+  tempPosition /= 1;
+
+  QtrPosition = tempPosition;
+  input = QtrPosition;
   
-  QtrPosition = qtr.readLineWhite(sensorValues);
+
 
   // print the sensor values as numbers from 0 to 1000, where 0 means maximum
   // reflectance and 1000 means minimum reflectance, followed by the line
   // position
-  for (uint8_t i = 0; i < SensorCount; i++)
-  {
-    Serial.print(sensorValues[i]);
-    Serial.print('\t');
-  }
-  Serial.println(QtrPosition);
+  // for (uint8_t i = 0; i < SensorCount; i++)
+  // {
+  //   Serial.print(sensorValues[i]);
+  //   Serial.print('\t');
+  // }
+  // Serial.println(QtrPosition);
 
-  delay(50);
+  // delay(50);
 }
 
 bool isSeeingLine(){
@@ -276,39 +331,95 @@ bool isSeeingLine(){
   return false;
 }
 
-void followLine(){
+
+void followLinePID(){
   readLine();
   // Serial.println(position);
   int degree = 90;
   if(isSeeingLine && !isRampDetected){
-    // Serial.println("see");
-//TODO age dorost nemicharkhid ino bokonom jori ke age vasat bod hamon 90 daraje bashe age gheir bod atomatic map kone
-    if(QtrPosition <= 3000){
-      degree = map(QtrPosition, 0, 2500, 45, 90);
+    pid.Compute();
+
+    if(output < desiredPosition){
+      degree = map(output, 0, desiredPosition, 45, 90);
     }
-    else if(QtrPosition >= 4500){
-      degree = map(QtrPosition, 4000, 7000, 90, 135);
+    else if(output > desiredPosition){
+      degree = map(output, desiredPosition, 7000, 90, 135);
     }
     else{
-      // Serial.println("90");
       degree = 90;
     } 
+
     // degree = map(QtrPosition, 0, 7000, 45, 135);
   }
   else if(!isSeeingLine && isRampDetected){
     degree = 90;
   }
-  // else{
-  //   if(QtrPosition <= 3500){
-  //     degree = 45;
-  //   }
-  //   else if(QtrPosition > 3500){
-  //     degree = 135;
-  //   }
-  //   else{
-  //     degree = 90;
-  //   }
-  // }
+  else{
+    pid.Compute();
+
+    if(output < desiredPosition){
+      degree = map(output, 0, desiredPosition, 45, 90);
+    }
+    else if(output > desiredPosition){
+      degree = map(output, desiredPosition, 7000, 90, 135);
+    }
+    else{
+      degree = 90;
+    } 
+  }
+  //check the value
+  if(degree > 135){
+    degree = 135;
+  }
+  else if(degree < 45){
+    degree = 45;
+  }
+  if (degree >= 45 && degree <= 135)
+  {
+    if(degree > 85 && degree < 95)
+      speed = 30;
+    else
+      speed = 25;
+    servoMove(degree);
+  }
+  // delay(150);
+}
+
+
+
+void followLine(){
+  readLine();
+  // Serial.println(position);
+  int degree = 90;
+  if(isSeeingLine && !isRampDetected){
+
+    if(QtrPosition < 3000){
+      degree = map(QtrPosition, 0, 3000, 50, 90);
+    }
+    else if(QtrPosition > 4000){
+      degree = map(QtrPosition, 4000, 7000, 95, 135);
+    }
+    // else{
+    //   degree = 90;
+    // } 
+
+
+    // degree = map(QtrPosition, 0, 7000, 45, 135);
+  }
+  else if(!isSeeingLine && isRampDetected){
+    degree = 90;
+  }
+  else{
+    if(QtrPosition < 3000){
+      degree = map(QtrPosition, 0, 3000, 45, 90);
+    }
+    else if(QtrPosition > 4000){
+      degree = map(QtrPosition, 4000, 7000, 90, 135);
+    }
+    // else{
+    //   degree = 90;
+    // } 
+  }
   //check the value
   if(degree > 135){
     degree = 135;
@@ -320,14 +431,16 @@ void followLine(){
   // Serial.println(degree);
   if (degree >= 45 && degree <= 135)
   {
-    if(degree == 90)
-      speed = 30;
+    if(degree < 80 && degree > 100)
+      speed = 28;
     else
       speed = 25;
     servoMove(degree);
   }
   // delay(150);
 }
+
+#pragma region tof_useless
 
 void tofSetIds(){
   // all reset
@@ -409,6 +522,7 @@ void readTofsensors() {
   Serial.println();
 }
 
+#pragma endregion
 
 void detectObject(){
   bool result = false;
@@ -417,14 +531,18 @@ void detectObject(){
     readTofsensors();
     int val1 = measure1.RangeMilliMeter;
     int val2 = measure2.RangeMilliMeter;
-    // Serial.println(val1);
-    // Serial.println(val2);
 
+
+    // long val1 = ultra1GetDistance();
+    // long val2 = ultra2GetDistance();
+
+    Serial.println(val1);
+    Serial.println(val2);
 
 
     if(val2 < objectDistance
         && val1 < objectDistance
-        && val1 - val2 < tofObjectDif
+        // && val1 - val2 < tofObjectDif
         && !isRampDetected
         ){
           result = true;
@@ -466,10 +584,13 @@ void loopTheObject(){
 }
 
 bool endOfLine(){
-  readLine();
-  for(int i = 0;i < SensorCount ; i++){
-     if(sensorValues[i] > qtrMinValForWhiteLine) return false;
+  for(int j = 0;j<2;j++){
+    readLine();
+    for(int i = 0;i < SensorCount ; i++){
+      if(sensorValues[i] > 600) return false;
+    }
   }
+  stopCar = true;
   return true;
 }
 
@@ -481,7 +602,7 @@ void detectRamp(){
     return;
   }
 
-  for(int i = 0;i< 2;i++)
+  for(int i = 0;i< 3;i++)
   {
     readTofsensors();
     int val1 = measure1.RangeMilliMeter;
@@ -491,11 +612,14 @@ void detectRamp(){
     // Serial.println("------------------------------->");
 
 
+    // long val1 = ultra1GetDistance();
+    // long val2 = ultra2GetDistance();
 
     if(val2 < rampDistance
         && val1 < rampDistance
+        && val2 != 0
         && !isObjectDetected
-        && val1 - val2 > tofRampdif
+        // && val1 - val2 > tofRampdif
         )
     {
       rampDurationStart = millis();
@@ -518,24 +642,48 @@ void startCar(){
 //here we check if object detected we will loop the object untile loop the object become false which means we passed the object
 //if we didnt detect the object we will detect it
   // isObjectDetected = isObjectDetected ? loopTheObject() : detectObject();
+  endOfLine();
+  // Serial.println("start car");
+
   if(isObjectDetected){
     loopTheObject();
+    // Serial.println("loop object");
     
   }
   else{
     // detectObject();
-    detectRamp();
+    // detectRamp();
   }
 
   // Serial.print(isObjectDetected);
-  Serial.println(speed);
+  // Serial.println(speed);
   if(!isObjectDetected)  followLine();
 
   speed = isRampDetected  ? 80 : speed;
+  
+  if(stopCar){
+    motorStop();
+  }
+  else{
+    motorGo(speed);
+
+  }
 
   // if(!endOfLine()) motorGo(speed);
 
 }
+
+// void readButton() {
+//   // Read the button state
+//   bool currentButtonState = digitalRead(BUTTON_PIN);
+
+//   // Check if the button state has changed
+//   if (currentButtonState != lastButtonState) {
+//     // Update the button state
+//     stopCar = !stopCar;
+//     delay(15);
+//   }
+// }
 
 void setup() {
   // put your setup code here, to run once:
@@ -546,7 +694,9 @@ void setup() {
   activeCalibraton ?  qtrSensorInit() : qtrSensorInitWithoutCal();
   servoInit();
   tofInit();
-  // motorGo(speed);
+  // pinMode(BUTTON_PIN, INPUT);    // Set the button pin as input
+
+  motorGo(25);
 
 }
 void loop() {
@@ -557,14 +707,21 @@ void loop() {
   // followLine();
 
   // motorGo(speed);
-
+  // readButton();
   startCar();
-  // detectObject();
 
-  // Serial.println(isObjectDetected);
+  //  stopCar ? motorStop() : startCar();
+  // detectObject();
+  // long dis1 =  ultra1GetDistance();
+  // long dis2 =  ultra2GetDistance();
+
+
+  // Serial.println(dis1);
+  // Serial.println(dis2);
+
 
   // readTofsensors();
-  delay(150);
+  // delay(250);
 
 
 }
